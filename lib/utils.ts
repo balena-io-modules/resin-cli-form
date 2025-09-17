bd47b0dff5dce324115e42a770ebd2b3f2604871
Thodoris Greasidis <thgreasi@gmail.com>
2025-09-17 18:13:58 +0300

    Decaffeinate all sources

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,4 +1,4 @@
-###
+/*
 Copyright 2016 Resin.io
 
 Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,115 +12,121 @@ distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
-###
+*/
 
-Promise = require('bluebird')
-_ = require('lodash')
-inquirer = require('inquirer')
+const Promise = require('bluebird');
+const _ = require('lodash');
+const inquirer = require('inquirer');
 
-###*
-# @summary Flatten form groups
-# @function
-# @protected
-#
-# @param {Object[]} form - form description
-# @returns {Object[]} flattened form description
-#
-# @example
-# questions = utils.flatten [
-# 	isGroup: true
-# 	name: 'network'
-# 	message: 'Network'
-# 	isCollapsible: true
-# 	collapsed: false
-# 	options: [
-# 		message: 'Network Connection'
-# 		name: 'network'
-# 		type: 'list'
-# 		choices: [ 'ethernet', 'wifi' ]
-# ,
-# 	message: 'Processor'
-# 	name: 'processorType'
-# 	type: 'list'
-# 	choices: [ 'Z7010', 'Z7020' ]
-# ]
-###
-exports.flatten = (form) ->
-	return _.flatMap form, (question) ->
-		if question.isGroup
-			return exports.flatten(question.options)
-		return question
+/**
+ * @summary Flatten form groups
+ * @function
+ * @protected
+ *
+ * @param {Object[]} form - form description
+ * @returns {Object[]} flattened form description
+ *
+ * @example
+ * questions = utils.flatten [
+ * 	isGroup: true
+ * 	name: 'network'
+ * 	message: 'Network'
+ * 	isCollapsible: true
+ * 	collapsed: false
+ * 	options: [
+ * 		message: 'Network Connection'
+ * 		name: 'network'
+ * 		type: 'list'
+ * 		choices: [ 'ethernet', 'wifi' ]
+ * ,
+ * 	message: 'Processor'
+ * 	name: 'processorType'
+ * 	type: 'list'
+ * 	choices: [ 'Z7010', 'Z7020' ]
+ * ]
+ */
+exports.flatten = form => _.flatMap(form, function(question) {
+    if (question.isGroup) {
+        return exports.flatten(question.options);
+    }
+    return question;
+});
 
-###*
-# @summary Parse a form definition
-# @function
-# @protected
-#
-# @param {Object[]} form - form description
-# @returns {Object[]} parsed questions
-#
-# @example
-# questions = utils.parse [
-# 	message: 'Processor'
-# 	name: 'processorType'
-# 	type: 'list'
-# 	choices: [ 'Z7010', 'Z7020' ]
-# ,
-# 	message: 'Coprocessor cores'
-# 	name: 'coprocessorCore'
-# 	type: 'list'
-# 	values: [ '16', '64' ]
-# ]
-###
-exports.parse = (form) ->
-	form = exports.flatten(form)
+/**
+ * @summary Parse a form definition
+ * @function
+ * @protected
+ *
+ * @param {Object[]} form - form description
+ * @returns {Object[]} parsed questions
+ *
+ * @example
+ * questions = utils.parse [
+ * 	message: 'Processor'
+ * 	name: 'processorType'
+ * 	type: 'list'
+ * 	choices: [ 'Z7010', 'Z7020' ]
+ * ,
+ * 	message: 'Coprocessor cores'
+ * 	name: 'coprocessorCore'
+ * 	type: 'list'
+ * 	values: [ '16', '64' ]
+ * ]
+ */
+exports.parse = function(form) {
+	form = exports.flatten(form);
 
-	return _.map form, (option) ->
+	return _.map(form, function(option) {
 
-		# We omit `when` since we internally translate this
-		# into a `shouldPrompt` function instead.
-		result = _.omit(_.cloneDeep(option), 'when')
+		// We omit `when` since we internally translate this
+		// into a `shouldPrompt` function instead.
+		const result = _.omit(_.cloneDeep(option), 'when');
 
-		# Translate object "when" definitions
-		# to functions we can run and evaluate
-		if not _.isEmpty(option.when)
-			result.shouldPrompt = (answers) ->
-				return false if not answers?
+		// Translate object "when" definitions
+		// to functions we can run and evaluate
+		if (!_.isEmpty(option.when)) {
+			result.shouldPrompt = function(answers) {
+				if ((answers == null)) { return false; }
 
-				return _.every option.when, (value, key) ->
-					answer = _.get(answers, key)
+				return _.every(option.when, function(value, key) {
+					const answer = _.get(answers, key);
 
-					# Evaluate `true` as an existencial operator
-					if value is true and Boolean(answer)
-						return true
+					// Evaluate `true` as an existencial operator
+					if ((value === true) && Boolean(answer)) {
+						return true;
+					}
 
-					return answer is value
+					return answer === value;
+				});
+			};
+		}
 
-		return result
+		return result;
+	});
+};
 
-###*
-# @summary Prompt a questions form
-# @function
-# @protected
-#
-# @param {Object[]} questions - form questions
-# @returns {Promise<Object>} answers
-#
-# @example
-# utils.prompt [
-# 	message: 'Processor'
-# 	name: 'processorType'
-# 	type: 'list'
-# 	choices: [ 'Z7010', 'Z7020' ]
-# ,
-# 	message: 'Coprocessor cores'
-# 	name: 'coprocessorCore'
-# 	type: 'list'
-# 	values: [ '16', '64' ]
-# ]
-# .then (answers) ->
-# 	console.log(answers.processorType)
-# 	console.log(answers.coprocessorCore)
-###
-exports.prompt = Promise.method (questions) ->
-	inquirer.prompt(questions)
+/**
+ * @summary Prompt a questions form
+ * @function
+ * @protected
+ *
+ * @param {Object[]} questions - form questions
+ * @returns {Promise<Object>} answers
+ *
+ * @example
+ * utils.prompt [
+ * 	message: 'Processor'
+ * 	name: 'processorType'
+ * 	type: 'list'
+ * 	choices: [ 'Z7010', 'Z7020' ]
+ * ,
+ * 	message: 'Coprocessor cores'
+ * 	name: 'coprocessorCore'
+ * 	type: 'list'
+ * 	values: [ '16', '64' ]
+ * ]
+ * .then (answers) ->
+ * 	console.log(answers.processorType)
+ * 	console.log(answers.coprocessorCore)
+ */
+exports.prompt = Promise.method(questions => inquirer.prompt(questions));
